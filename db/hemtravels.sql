c3b7ebe426c926fcee74e8ab9eecffa6c417f316
ANJANEYA-PC\Anjaneyavadivel <anjaneyavaduvel@gmail.com>
2018-07-23 18:37:37 +0530

    Group update

--- db/hemtravels.sql
+++ db/hemtravels.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2018 at 03:22 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Jul 23, 2018 at 03:06 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,10 +23,203 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account_info`
+--
+
+CREATE TABLE IF NOT EXISTS `account_info` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `bank_name` varchar(150) NOT NULL,
+  `account_holder_name` varchar(150) NOT NULL,
+  `account_number` varchar(150) NOT NULL,
+  `ifsc_code` varchar(50) NOT NULL,
+  `branch` varchar(150) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `is_primary` int(1) NOT NULL DEFAULT '0' COMMENT '0 - not primary, 1 - primary',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city_master`
+--
+
+CREATE TABLE IF NOT EXISTS `city_master` (
+  `id` int(11) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_us` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `subject` tinytext NOT NULL,
+  `message` tinytext NOT NULL,
+  `is_read` int(1) NOT NULL DEFAULT '0' COMMENT '0 - not read, 1 - read',
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country_master`
+--
+
+CREATE TABLE IF NOT EXISTS `country_master` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq_master`
+--
+
+CREATE TABLE IF NOT EXISTS `faq_master` (
+  `id` int(11) NOT NULL,
+  `trip_id` int(11) NOT NULL DEFAULT '0' COMMENT '0 - admin common FAQ',
+  `question` tinytext NOT NULL,
+  `answer` tinytext NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `message` tinytext NOT NULL,
+  `notified` int(1) NOT NULL DEFAULT '0' COMMENT '0 - not read, 1 - read',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state_master`
+--
+
+CREATE TABLE IF NOT EXISTS `state_master` (
+  `id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip_category`
+--
+
+CREATE TABLE IF NOT EXISTS `trip_category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(11) NOT NULL COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip_inclusions`
+--
+
+CREATE TABLE IF NOT EXISTS `trip_inclusions` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `trip_tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_address`
+--
+
+CREATE TABLE IF NOT EXISTS `user_address` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address1` varchar(150) NOT NULL,
+  `address2` varchar(150) NOT NULL,
+  `street` varchar(150) NOT NULL,
+  `landmark` varchar(150) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `zip_code` int(8) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `is_primary` int(1) NOT NULL DEFAULT '0' COMMENT '0 - not primary, 1 - primary',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_master`
 --
 
-CREATE TABLE `user_master` (
+CREATE TABLE IF NOT EXISTS `user_master` (
   `id` int(11) NOT NULL,
   `user_type` varchar(2) NOT NULL DEFAULT 'CU' COMMENT 'SA - super admin, VA - Vendor, CU - Customer, GU - Guest, OB - Office booking',
   `email` varchar(60) DEFAULT NULL,
@@ -48,18 +241,103 @@ CREATE TABLE `user_master` (
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_visit` datetime DEFAULT NULL,
   `isactive` tinyint(1) NOT NULL DEFAULT '2' COMMENT '0->de active, 1->active,2->not activated'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_master`
 --
 
 INSERT INTO `user_master` (`id`, `user_type`, `email`, `password`, `user_fullname`, `dob`, `gender`, `phone`, `alt_phone`, `emergency_contact_person`, `emergency_contact_no`, `activation_code`, `activation_code_time`, `about_me`, `profile_pic`, `fb_id`, `google_id`, `login_via`, `created_on`, `last_visit`, `isactive`) VALUES
-(1, 'CU', 'kumar@gmail.com', '14e1b600b1fd579f47433b88e8d85291', 'karthi', '2018-06-22 00:00:00', '1', '9688079118', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 1, '2018-06-30 18:50:09', NULL, 2);
+(1, 'CU', 'customer@gmail.com', '14e1b600b1fd579f47433b88e8d85291', 'Customer', '2018-06-22 00:00:00', '1', '9688079118', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 1, '2018-06-30 18:50:09', NULL, 1),
+(2, 'SA', 'admin@gmail.com', '14e1b600b1fd579f47433b88e8d85291', 'Admin', '2018-06-22 00:00:00', '1', '9688079118', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 1, '2018-06-30 18:50:09', NULL, 1),
+(3, 'VA', 'vendor@gmail.com', '14e1b600b1fd579f47433b88e8d85291', 'Vendor', '2018-06-22 00:00:00', '1', '9688079118', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 1, '2018-06-30 18:50:09', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_wishlist`
+--
+
+CREATE TABLE IF NOT EXISTS `user_wishlist` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `trip_id` int(11) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` int(11) NOT NULL,
+  `isactive` int(1) NOT NULL DEFAULT '1' COMMENT '0 - Deactive, 1 - Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `account_info`
+--
+ALTER TABLE `account_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city_master`
+--
+ALTER TABLE `city_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `country_master`
+--
+ALTER TABLE `country_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faq_master`
+--
+ALTER TABLE `faq_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `state_master`
+--
+ALTER TABLE `state_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trip_category`
+--
+ALTER TABLE `trip_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trip_inclusions`
+--
+ALTER TABLE `trip_inclusions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trip_tags`
+--
+ALTER TABLE `trip_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_address`
+--
+ALTER TABLE `user_address`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_master`
@@ -68,14 +346,80 @@ ALTER TABLE `user_master`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_wishlist`
+--
+ALTER TABLE `user_wishlist`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `account_info`
+--
+ALTER TABLE `account_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `city_master`
+--
+ALTER TABLE `city_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `country_master`
+--
+ALTER TABLE `country_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `faq_master`
+--
+ALTER TABLE `faq_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `state_master`
+--
+ALTER TABLE `state_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `trip_category`
+--
+ALTER TABLE `trip_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `trip_inclusions`
+--
+ALTER TABLE `trip_inclusions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `trip_tags`
+--
+ALTER TABLE `trip_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_address`
+--
+ALTER TABLE `user_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user_master`
 --
 ALTER TABLE `user_master`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `user_wishlist`
+--
+ALTER TABLE `user_wishlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
