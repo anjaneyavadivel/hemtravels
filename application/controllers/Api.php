13a5561d6fef433ddb2de6674344f8ca1be31dc2
BHUVI <BHUVI@BHUVI-PC>
2018-08-08 09:41:27 +0530

    8-8-2018-google-login

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -24,73 +24,8 @@ public $data;
 	public function index()
 	{
 	}
+	
 	public function gmail()
-	{
-		
-		// Include two files from google-php-client library in controller
-		include_once APPPATH . "libraries/google/src/Google/Client.php";
-		include_once APPPATH . "libraries/google/src/Google/Service/Oauth2.php";
-		include_once APPPATH . "libraries/google/src/Google/autoload.php";
-		
-		// Store values in variables from project created in Google Developer Console
-		$client_id 		= '176321012278-aedadjf8eb0v6ptgomn3rvh5lho2am1b.apps.googleusercontent.com';
-		$client_secret 	= 'krA_aoQAPo1OFBa0_6186Tdk';
-		$redirect_uri 	= 'http://goatravelagent.com/gmail';//if this url change means google developer console url also changed
-		$simple_api_key = 'AIzaSyBrE62QSp_ijbM30EaEtKn2H62XonGMQcY';
-		
-		// Create Client Request to access Google API
-		$client 		= new Google_Client();
-		$client->setApplicationName("PHP Google OAuth Login Example");
-		$client->setClientId($client_id);
-		$client->setClientSecret($client_secret);
-		$client->setRedirectUri($redirect_uri);
-		$client->setDeveloperKey($simple_api_key);
-		$client->addScope("https://www.googleapis.com/auth/userinfo.email");
-		
-		// Send Client Request
-		$objOAuthService = new Google_Service_Oauth2($client);
-		
-		// Add Access Token to Session
-		if (isset($_GET['code'])) {
-		$client->authenticate($_GET['code']);
-		$this->session->set_userdata['access_token'] = $client->getAccessToken();
-		header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-		}
-		
-		// Set Access Token to make Request
-		if (isset($this->session->set_userdata['access_token']) && $this->session->set_userdata['access_token']) {
-		$client->setAccessToken($this->session->set_userdata['access_token']);
-		}
-		
-		// Get User Data from Google and store them in $data
-		if ($client->getAccessToken()) {
-		$userData = $objOAuthService->userinfo->get();
-		$data['userData'] = $userData;
-		$this->session->set_userdata['access_token'] = $client->getAccessToken();
-		
-		
-		
-		$userProfile = $objOAuthService->userinfo->get();
-		print_r($userProfile);exit;
-		//calling function for udate r inserting records to our db.
-	$this->checkUser('google',$userProfile['id'],$userProfile['given_name'],$userProfile['family_name'],$userProfile['email'],$userProfile['gender'],$userProfile['locale'],$userProfile['link'],$userProfile['picture']);
-	$this->session->userdata['google_data'] = $userProfile; // Storing Google User Data in Session
-		
-		} 
-		else 
-		{
-			$authUrl = $client->createAuthUrl();
-			$data['authUrl'] = $authUrl;
-		}
-		if(isset($data['authUrl']))
-			redirect($authUrl);//this is redirect to google in this place
-		else
-		{
-			$this->session->set_userdata('error','You have some error in Goole with Login please try another');
-			redirect (base_url());
-		}
-	}
-	public function gmail1()
 	{
 		// Include two files from google-php-client library in controller
 		include_once APPPATH . 'libraries/google/src/Google_Client.php';
@@ -104,7 +39,7 @@ public $data;
 		if ($this->input->get('code')) 
 		{
 		  	$client->authenticate($this->input->get('code'));
-		 	 $_SESSION['token'] = $client->getAccessToken();
+		 	// $_SESSION['token'] = $client->getAccessToken();
 		  	if ($client->getAccessToken()) 
 			{
 				$user = $oauth2->userinfo->get();
@@ -112,9 +47,12 @@ public $data;
 				$name = explode(" ",$content['name']);
 				$fname = isset($name[0])?$name[0]:'';
 				$lname = isset($name[1])?$name[1]:'';
-				print_r($content);exit;
-				$this->check_data('gmail',$content['id'],$fname,$lname,$content['picture']);
+				$email=isset($content['email'])?$content['email']:'';
+				//print_r($content);exit;
+				$this->check_data('gmail',$content['id'],$fname,$lname,$content['picture'],$email);
+				exit;
 				$_SESSION['token'] = $client->getAccessToken();
+				exit;
 			} 
 			else
 			{
@@ -185,17 +123,18 @@ public $data;
 			?> <a href="<?php echo $login; ?>">Login</a><?php
 		}
 	}
-	function check_data($net,$id,$fname,$lname,$profile_image_url)
+	function check_data($net,$id,$fname,$lname,$profile_image_url,$email='')
 	{
 		$check						=	$this->user_model->select('user_master',array('social_network'=>$net,'auth_id'=>$id));
+		//echo $this->db->last_query();exit;
 		if($check->num_rows()>0)
 		{
 			$ch		=	$check->row();
-			$this->session->set_userdata('user_id',$ch->um_user_id);
-			$this->session->set_userdata('user_email',$ch->um_email);
-			$this->session->set_userdata('name',$ch->um_first_name.' '.$ch->um_first_name);
-			$this->session->set_userdata('user_img',$ch->um_profile_pic);
-			$this->session->set_userdata('user_type',$ch->um_type);
+			$this->session->set_userdata('user_id',$ch->id);
+			$this->session->set_userdata('user_email',$ch->email);
+			$this->session->set_userdata('name',$ch->user_fullname);
+			$this->session->set_userdata('user_img',$ch->profile_pic);
+			$this->session->set_userdata('user_type',$ch->user_type);
 			if($this->session->userdata('last_url'))
 			{				
 				redirect();
@@ -209,29 +148,15 @@ public $data;
 		else
 		{
 			
-			//echo $this->session->userdata('signup_socail').'******';exit;
-			if($this->session->userdata('signup_socail')==1)
-			{				
-				/*$this->session->set_userdata('signup_socail',0);
-				$this->session->unset_userdata('signup_socail');*/
-				$this->session->set_userdata('social_network',$net);
-				$this->session->set_userdata('auth_id',$id);
-				$this->session->set_userdata('first_name',$fname);
-				$this->session->set_userdata('last_name',$lname);
-				$this->session->set_userdata('user_img',$profile_image_url);
-				//echo "hi";exit;
-				$this->session->set_userdata('suc','Login Successfully...!');
-				redirect();
-			}
-			if($this->session->userdata('login_socail')==1)
-			{
+			
 				$values	=	array('social_network'	=>	$net,
 								'auth_id'			=>	$id,
+								'email'			=>	$email,
 								'user_fullname'			=>	$fname.' '.$lname,
-								'um_type'				=>	'CU',
-								'um_profile_pic'		=>	$profile_image_url);
+								'user_type'				=>	'CU',
+								'profile_pic'		=>	$profile_image_url);
 								
-				$query	=	$this->usermodel->insert('user_master',$values);
+				$query	=	$this->user_model->insert('user_master',$values);
 				//echo $this->db->last_query();exit;
 				$last_id=	$this->db->insert_id();
 				if($query)
@@ -242,11 +167,11 @@ public $data;
 						/*$this->session->set_userdata('login_socail',0);
 						$this->session->unset_userdata('login_socail');*/
 						$ch		=	$check->row();
-						$this->session->set_userdata('user_id',$ch->um_user_id);
-						$this->session->set_userdata('user_email',$ch->um_email);
-						$this->session->set_userdata('name',$ch->um_first_name.' '.$ch->um_first_name);
-						$this->session->set_userdata('user_img',$ch->um_profile_pic);
-						$this->session->set_userdata('user_type',$ch->um_type);
+						$this->session->set_userdata('user_id',$ch->id);
+						$this->session->set_userdata('user_email',$ch->email);
+						$this->session->set_userdata('name',$ch->user_fullname);
+						$this->session->set_userdata('user_img',$ch->profile_pic);
+						$this->session->set_userdata('user_type',$ch->user_type);
 						
 						$this->session->set_userdata('suc','Login Successfully...!');
 						redirect();
@@ -262,7 +187,7 @@ public $data;
 					$this->session->set_userdata('err','Please try again..!');
 					redirect();
 				}
-			}
+			
 		}
 	}
 	
