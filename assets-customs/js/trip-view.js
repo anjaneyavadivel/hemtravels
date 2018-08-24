bf90303596cf91d6e02bb61b322d3b2eb99f40dc
ANJANEYA-PC\Anjaneyavadivel <anjaneyavaduvel@gmail.com>
2018-08-25 01:06:35 +0530

    bug fix

--- assets-customs/js/trip-view.js
+++ assets-customs/js/trip-view.js
@@ -55,25 +55,27 @@ jQuery(function($) {
     
     
     $('#rangeDatePicker > div > div').dateRangePicker({
-            separator : ' to ',
+            //separator : ' to ',
             autoClose: true,
             format: 'MMM D, YYYY',
-            minDays: 4,
-            maxDays: 4,
-            stickyMonths: true,
-            startDate: new Date(),
-            showTopbar: false,
-            getValue: function()
-            {
-                    if ($('#rangeDatePickerTo').val() && $('#rangeDatePickerFrom').val() )
-                            return $('#rangeDatePickerTo').val() + ' to ' + $('#rangeDatePickerFrom').val();
-                    else
-                            return '';
-            },
+            singleDate : true,
+            showShortcuts: false,
+//            minDays: 4,
+//            maxDays: 4,
+//            stickyMonths: true,
+//            //startDate: new Date(),
+//            showTopbar: false,
+//            getValue: function()
+//            {
+//                    if ($('#rangeDatePickerTo').val() && $('#rangeDatePickerFrom').val() )
+//                            return $('#rangeDatePickerTo').val() + ' to ' + $('#rangeDatePickerFrom').val();
+//                    else
+//                            return '';
+//            },
             setValue: function(s,s1,s2)
             {
                     $('#rangeDatePickerFrom').val(s1);
-                    $('#rangeDatePickerTo').val(s2);
+                    //$('#rangeDatePickerTo').val(s2);
             },
             beforeShowDay: function(t){ //console.log(t);
                    // var valid = !(t.getDate() == 5 || t.getDate() == 17 || t.getDate() == 18 || t.getDate() == 19  || t.getDate() == 26 );
